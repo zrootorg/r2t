9bbe9416a3783e509e98a13219ec6b4e9f37d205
Manuel Bovo <manuel.bovo@gmail.com>
2023-07-09 22:59:53 +0200

    Fix feedparser

--- src/r2t/feedparser.py
+++ src/r2t/feedparser.py
@@ -21,42 +21,57 @@ import feedparser
 from typing import Dict,List
 import threading
 import logging
+from .telegrambot import TelegramBot
 
 class Parser(object):
     feeds: List = list()
     db: DB = None
+    bot: TelegramBot
 
-    def __init__(self, feeds:List[Dict[str,str]], db: DB, stop_event: threading.Event)-> None:
+    def __init__(self, feeds:List[Dict[str,str]], db: DB, stop_event: threading.Event, bot: TelegramBot)-> None:
         self.feeds = feeds
         self.db = db
         self.stop_event = stop_event
+        self.bot = bot
 
     def list_feeds(self):
         return self.feeds
 
     def parse(self):
-      logging.info("Start parsing feeds")
-      for feed, url in self.feeds.items():
+        logging.info("Start parsing feeds")
+        counters = {
+            "total": 0,
+            "new": 0,
+            "skipped": 0
+        }
+        for feed, url in self.feeds.items():
             logging.info(f"Parse feed {feed} {url}")
             if self.stop_event.is_set():
                 logging.info("Stop event, quit")
                 break
             self.db.create_table(feed)
             parsed_feed = feedparser.parse(url)
-            for article in sorted(
-                parsed_feed["entries"],
-                key=lambda k: k.published,
-                reverse=False
-            ):
+
+            if parsed_feed.bozo:
+                logging.error(f"Error parsing feed {feed} {url}: {parsed_feed.bozo_exception}")
+                continue
+            entries = sorted(parsed_feed.entries, key=lambda k: k.published, reverse=False)
+            counters["total"] += len(entries)
+            for article in entries:
                 if self.stop_event.is_set():
                     logging.info("Stop event, quit")
                     break
-                if self.db.is_found(feed, article["title"], article["updated"]):
-                    logging.info(f"Skip item {feed} {article['title']} {article['updated']}")
+                title = article.title
+                # link = article.link
+                updated = article.updated
+                if self.db.is_found(feed, title, updated):
+                    logging.debug(f"Skip item {feed} {title} {updated}")
+                    counters["skipped"] += 1
                     continue
-                logging.info(f"Add item {feed} {article['title']} {article['updated']}")
-                self.db.add_item(feed, article["title"], article["updated"])
-
+                logging.debug(f"New item {feed} {title} {updated}")
+                self.db.add_item(feed, title, updated)
+                counters["new"] += 1
+        logging.info(f"New items: {counters['new']} - Skipped items: {counters['skipped']} - Total items: {counters['total']}")
     def stop(self):
         self.db.close()
         pass
